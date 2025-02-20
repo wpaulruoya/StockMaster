0693fcbb41d7b29e988fd2142e02b377bb62cf68
pruoya@gmail.com <pruoya@gmail.com>
2025-02-20 17:14:17 +0300

    Added Admin pages

---
+++ Models/AdminViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+
+namespace StockMaster.Models
+{
+    public class AdminViewModel
+    {
+        public List<IdentityUser> Users { get; set; } = new List<IdentityUser>();
+        public int TotalUsers { get; set; }
+        public int TotalInventoryItems { get; set; }
+        public int PendingOrders { get; set; } // Placeholder (Extendable)
+    }
+}
