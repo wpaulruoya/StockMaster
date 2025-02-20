367dafd1c4389704e8243be114080985c6a353f8
pruoya@gmail.com <pruoya@gmail.com>
2025-02-20 14:47:53 +0300

    About to configure manage inventoryy page

---
+++ Models/InventoryViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace StockMaster.Models
+{
+    public class InventoryViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public string UserId { get; set; }
+        public string UserEmail { get; set; } // ✅ Store user's email
+    }
+}
