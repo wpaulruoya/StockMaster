f5365f77c53a9cd460e6c53ba871861f5d18c31d
pruoya@gmail.com <pruoya@gmail.com>
2025-03-12 10:50:18 +0300

    Refactoring the Inventory Controller into its respective service and interface

---
+++ Application Layer/Interfaces/IInventoryService.cs
@@ -0,0 +1,12 @@
+﻿using StockMaster.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+public interface IInventoryService
+{
+    Task<List<Inventory>> GetUserInventoryAsync(string userId);
+    Task<Inventory> GetInventoryByIdAsync(int id, string userId);
+    Task<bool> AddInventoryAsync(Inventory inventory);
+    Task<bool> UpdateInventoryAsync(Inventory inventory);
+    Task<bool> DeleteInventoryAsync(int id, string userId);
+}
